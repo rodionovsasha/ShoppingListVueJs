a404f8411e30ce747227f69c2235b514b7eb9c08
Rodionov Aleksandr <allrod@mail.ru>
2018-04-29 14:29:42 +0300

    Add index page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 // import ShoppingList from './components/ShoppingList'
-// import router from './router'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -22,7 +22,7 @@ Vue.component('listItem', {
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   components: {App},
   template: '<App/>',
   data: {
